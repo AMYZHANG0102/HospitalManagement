d3f1e193f8c297d18be7d2fa1605df65a1749b8e
AMYZHANG0102 <amyzhc0102@gmail.com>
2025-11-06 14:19:24 -0500

    deleted some uneccessary dtos

--- HospitalManagement.Core/DTOs/DoctorReadDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using HospitalManagement.Core.Models;
-namespace TaskManagement.Core.DTOs;
-
-public class DoctorReadDto : UserReadDto
-{
-    [Required (ErrorMessage = "Specialization must be stated")]
-    public Specialization Specialization { get; set; }
-}
--- HospitalManagement.Core/DTOs/PatientReadDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace TaskManagement.Core.DTOs;
-
-public class PatientReadDto : UserReadDto
-{
-    [Required(ErrorMessage = "Health card is required")]
-    [RegularExpression(@"^\d{4}-\d{3}-\d{3}$", ErrorMessage = "Healthcard must be in format xxxx-xxx-xxx")]
-    public string HealthCard { get; set; } = string.Empty;
-}
