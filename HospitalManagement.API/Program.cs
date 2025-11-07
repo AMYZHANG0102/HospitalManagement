a2bd46f31a3b5364c688990a5f97909ab2472b5e
Hira <ahmadhira8@gmail.com>
2025-11-07 03:22:25 -0500

    Update Program.cs

--- HospitalManagement.API/Program.cs
+++ HospitalManagement.API/Program.cs
@@ -1,39 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using HospitalManagement.Core.Interfaces;
+using HospitalManagement.Core.Models;
+using HospitalManagement.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//map the controllers and inject the dependencies
+
+
+// Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson(); // Required for JSON Patch support
+// Configure SQLite Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Register Repository
+builder.Services.AddScoped<IPatientRepository, PatientRepository>(); //dependency injection for patient repository
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+app.MapOpenApi();
 }
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
