ab7264f8ed37bddb5cbeb34473810fb5af31ae27
AMYZHANG0102 <amyzhc0102@gmail.com>
2025-11-30 22:15:21 -0500

    created blazor server

--- HospitalManagement.Core/Models/User.cs
+++ HospitalManagement.Core/Models/User.cs
@@ -5,47 +5,38 @@ public class User
 {
     public long Id { get; set; }
 
-    [Required (ErrorMessage = "First Name is required")]
-    [StringLength (50, ErrorMessage = "First Name cannot exceed 50 characters")]
+    [Required(ErrorMessage = "First Name is required")]
+    [StringLength(50, ErrorMessage = "First Name cannot exceed 50 characters")]
     public string FirstName { get; set; } = string.Empty;
 
-    [Required (ErrorMessage = "Last Name is required")]
-    [StringLength (50, ErrorMessage = "Last Name cannot exceed 50 characters")]
+    [Required(ErrorMessage = "Last Name is required")]
+    [StringLength(50, ErrorMessage = "Last Name cannot exceed 50 characters")]
     public string LastName { get; set; } = string.Empty;
 
-    [Required (ErrorMessage = "User role needs to be specified")]
+    [Required(ErrorMessage = "User role needs to be specified")]
     public Role Role { get; set; }
 
-    [Required (ErrorMessage = "Phone is required")]
-    [RegularExpression (@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Phone must be in format xxx-xxx-xxxx")]
+    [Required(ErrorMessage = "Phone is required")]
+    [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Phone must be in format xxx-xxx-xxxx")]
     public string Phone { get; set; } = string.Empty;
 
-    [Required (ErrorMessage = "Email is required")]
-    [EmailAddress (ErrorMessage = "Invalid email format")]
-    [StringLength (100, ErrorMessage = "Address cannot exceed 100 characters")]
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    [StringLength(100, ErrorMessage = "Address cannot exceed 100 characters")]
     public string Email { get; set; } = string.Empty;
 
-    [Required (ErrorMessage = "Password is required")]
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = string.Empty;
 
-    [Required (ErrorMessage = "Gender is required")]
+    [Required(ErrorMessage = "Gender is required")]
     public Gender Gender { get; set; }
 
-    [Required (ErrorMessage = "Birthdate is required")]
+    [Required(ErrorMessage = "Birthdate is required")]
     public DateOnly Birthdate { get; set; }
 
-    [Required (ErrorMessage = "Home Address is required")]
-    [StringLength (100, ErrorMessage = "Home Address cannot exceed 100 characters")]
+    [Required(ErrorMessage = "Home Address is required")]
+    [StringLength(100, ErrorMessage = "Home Address cannot exceed 100 characters")]
     public string HomeAddress { get; set; } = string.Empty;
 
     public UserStatus Status { get; set; } = UserStatus.Inactive;
-
-    bool Login() {
-        Status = UserStatus.Active;
-        return true;
-    }
-
-    void Logout() {
-        Status = UserStatus.Inactive;
-    }
 }
