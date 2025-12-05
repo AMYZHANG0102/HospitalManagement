8f699ba719b580560079fee98e11ecd514c131ec
HAHMAD97 <ahmadhira8@gmail.com>
2025-12-05 00:41:00 -0500

    updated the homepage

---
+++ BlazorServer/Models/UserLoginModel.cs
@@ -0,0 +1,12 @@
+/* Hira Ahmad
+Summary: UserLoginModel class to represent user login data. */
+
+using System.ComponentModel.DataAnnotations;
+namespace BlazorServer.Models;
+
+public class UserLoginModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
